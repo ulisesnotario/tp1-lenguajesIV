1ef1fd878ec3fd5b2d00768292a694af6a0c953f
ulisesnotario <106499119+ulisesnotario@users.noreply.github.com>
2025-09-13 12:08:04 -0300

    Delete TP2 directory

--- TP2/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/tp1-lenguajesIV/TP2/", // Respetar minúsculas/mayúsculas igual a la URL
-});
